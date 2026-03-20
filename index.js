c22fb094a1b7f944b8f1d61754e07d4e725e3c6c
Norris Zhang <norris.zhang@sinch.com>
2026-03-21 11:10:10 +1300

    user joins the chat room

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
 
 const app = express();
 
-app.use(express.static('public'));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
-app.get('/chat', (req, res) => {
-    res.send('Welcome to chat');
+
+const chat = {
+    users: [],
+    history: []
+};
+
+app.post('/join', (req, res) => {
+    const nickname = req.body.nickname;
+    chat.users.push(nickname);
+    chat.history.push(`Welcome ${nickname} to join the chat.`);
+    res.render('chat', { chat });
+});
+
+app.get('/poll', (req, res) => {
+    res.send(JSON.stringify(chat));
 });
 
 
